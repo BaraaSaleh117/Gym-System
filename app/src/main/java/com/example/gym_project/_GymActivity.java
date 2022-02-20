e0cef46d0d68de1aac30df5a4174db59be8f0150
BaraaSaleh117 <baraasaleh117@hotmail.com>
2022-02-20 16:18:39 +0200

    LastCommit

--- app/src/main/java/com/example/gym_project/_GymActivity.java
+++ app/src/main/java/com/example/gym_project/_GymActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -15,6 +16,8 @@ public class _GymActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout._gym);
+        findViewById(R.id.port_btn).setVisibility(View.GONE);
+
 
     }
 
@@ -27,5 +30,20 @@ public class _GymActivity extends AppCompatActivity {
     public void employee(View view) {
         Intent intent = new Intent(_GymActivity.this,EmployeePart.class);
         startActivity(intent);
+    }
+
+    public void Landscape(View view) {
+        findViewById(R.id.port_btn).setVisibility(View.VISIBLE);
+        findViewById(R.id.land_btn).setVisibility(View.GONE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
+
+    }
+
+    public void Portrait(View view) {
+        findViewById(R.id.port_btn).setVisibility(View.GONE);
+        findViewById(R.id.land_btn).setVisibility(View.VISIBLE);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
     }
 }
